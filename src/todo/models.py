a9a355215c74f0f122d44c23c0557bfa44584e2a
Pratik Sah <shivam3448@gmail.com>
2024-11-04 00:53:12 +0530

    Added timstamps in todo model

--- src/todo/models.py
+++ src/todo/models.py
@@ -6,6 +6,8 @@ class TodoItem(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
