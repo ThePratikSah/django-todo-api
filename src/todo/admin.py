ead900808ebeddde1faeb05dae6aa5d21f1af2be
Pratik Sah <shivam3448@gmail.com>
2024-11-05 12:17:23 +0530

    Updated the fields to be shown for Todo Model

--- src/todo/admin.py
+++ src/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from todo.models import TodoItem
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "completed", "created_at"]
+
+
+admin.site.register(TodoItem, TodoAdmin)
